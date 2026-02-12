124cda679ca9e80cb37b945c5d4b776105023190
PedroAMoraes <pealcantara@hotmail.com>
2026-02-11 21:27:03 -0300

    Final push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,13 +15,13 @@ public class Main {
 
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso java");
-        curso1.setDescricao("descrição curso java");
+        curso1.setDescricao("Descrição curso java");
         curso1.setCargaHoraria(8);
 
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso js");
-        curso2.setDescricao("descrição curso js");
+        curso2.setDescricao("Descrição curso js");
         curso2.setCargaHoraria(4);
 
         System.out.println(curso1);
